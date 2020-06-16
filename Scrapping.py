48d6d9441967b47ac952f3c1a5c6e8ef224328af
johannesta <johannes.talero@gmail.com>
2020-06-15 20:14:18 -0500

    Brikss first observation

---
+++ Scrapping.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+import requests 
+import lxml
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+
+# Iniciar driver
+options=webdriver.ChromeOptions()
+options.add_argument('--incognito')
+
+#brikss.com
+driver= webdriver.Chrome(executable_path=r'H:/2020-02/How to find a new home with Scraping and game theory/Codigo/chromedriver.exe',options=options)
+url='https://www.brikss.com/residencial?o=arriendo'
+driver.get(url)
+Links_Apt=driver.find_elements_by_xpath('//div[@class="inmueblePreview cirugia"]')
+
+Lista_Urls=[]
+for link in Links_Apt:
+    try:
+        codigo_mobile=link.find_element_by_xpath('.//div[@class="codigo-mobile"]').text.split('\n')[1]
+        url_new='https://www.brikss.com/inmueble/'+codigo_mobile+'?o=comprar'
+        Lista_Urls.append(url_new)
+    except: 
+        pass        
+
+data=pd.DataFrame()
+
+for url_tem in Lista_Urls:
+    driver.get(url_tem)
+    chars1=driver.find_elements_by_xpath('.//div[@class="charItem"]')
+
+    Chara_url_temp=dict()
+    for char in chars1:
+        Chara_url_temp[char.text.split('\n')[1]]=char.text.split('\n')[0]
+    Chara_url_temp['URL']=url_tem
+    data_tem=pd.DataFrame(Chara_url_temp,index=0)
+
+
+
+
+
+
+
+
+driver.close
+
