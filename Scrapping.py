d3a02b291d14759a7b226491a8172a55120798c6
johannesta <johannes.talero@gmail.com>
2020-06-17 22:25:37 -0500

    Data frame for Brikss is working

--- Scrapping.py
+++ Scrapping.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import requests 
 import lxml
+import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
@@ -14,6 +15,7 @@ options.add_argument('--incognito')
 driver= webdriver.Chrome(executable_path=r'H:/2020-02/How to find a new home with Scraping and game theory/Codigo/chromedriver.exe',options=options)
 url='https://www.brikss.com/residencial?o=arriendo'
 driver.get(url)
+time.sleep(10)
 Links_Apt=driver.find_elements_by_xpath('//div[@class="inmueblePreview cirugia"]')
 
 Lista_Urls=[]
@@ -29,15 +31,15 @@ data=pd.DataFrame()
 
 for url_tem in Lista_Urls:
     driver.get(url_tem)
+    time.sleep(10)
     chars1=driver.find_elements_by_xpath('.//div[@class="charItem"]')
-
     Chara_url_temp=dict()
     for char in chars1:
-        Chara_url_temp[char.text.split('\n')[1]]=char.text.split('\n')[0]
-    Chara_url_temp['URL']=url_tem
-    data_tem=pd.DataFrame(Chara_url_temp,index=0)
-
-
+        Chara_url_temp.update({char.text.split('\n')[1]:[char.text.split('\n')[0]]})
+    Chara_url_temp.update({'URL':[url_tem]})
+    data_tem=pd.DataFrame.from_dict(Chara_url_temp)
+    data.append(date_tem)
+    print(data_tem)
 
 
 
