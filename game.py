97ea0eb5eb19ec9661b63eb96d7231fee1869d19
johannesta <johannes.talero@gmail.com>
2020-08-08 09:23:57 -0500

    End of simulation

--- game.py
+++ game.py
@@ -45,33 +45,33 @@ def a_against_b(a,b,won=0):
     None.
 
     """
-    A=Datos.iloc[a,]
-    B=Datos.iloc[b,]
+    A=Data.iloc[a,]
+    B=Data.iloc[b,]
 
     E_1,E_2=calculate_expected_score(A,B)
     #-------expected--------
-    Datos.loc[:,'score_ex'].iloc[a,]=Datos.loc[:,'score_ex'].iloc[a,] + E_1
-    Datos.loc[:,'score_ex'].iloc[b,]=Datos.loc[:,'score_ex'].iloc[b,] + E_2
+    Data.loc[:,'score_ex'].iloc[a,]=Data.loc[:,'score_ex'].iloc[a,] + E_1
+    Data.loc[:,'score_ex'].iloc[b,]=Data.loc[:,'score_ex'].iloc[b,] + E_2
 
     if won==0:
-        Datos.loc[:,'score_ac'].iloc[a,]=Datos.loc[:,'score_ac'].iloc[a,] + 0.5
-        Datos.loc[:,'score_ac'].iloc[b,]=Datos.loc[:,'score_ac'].iloc[b,] + 0.5
+        Data.loc[:,'score_ac'].iloc[a,]=Data.loc[:,'score_ac'].iloc[a,] + 0.5
+        Data.loc[:,'score_ac'].iloc[b,]=Data.loc[:,'score_ac'].iloc[b,] + 0.5
     elif won==1:
-        Datos.loc[:,'score_ac'].iloc[a,]=Datos.loc[:,'score_ac'].iloc[a,] + 1
-        Datos.loc[:,'score_ac'].iloc[b,]=Datos.loc[:,'score_ac'].iloc[b,] + 0
+        Data.loc[:,'score_ac'].iloc[a,]=Data.loc[:,'score_ac'].iloc[a,] + 1
+        Data.loc[:,'score_ac'].iloc[b,]=Data.loc[:,'score_ac'].iloc[b,] + 0
     elif won==2:
-        Datos.loc[:,'score_ac'].iloc[a,]=Datos.loc[:,'score_ac'].iloc[a,] + 0
-        Datos.loc[:,'score_ac'].iloc[b,]=Datos.loc[:,'score_ac'].iloc[b,] + 1
+        Data.loc[:,'score_ac'].iloc[a,]=Data.loc[:,'score_ac'].iloc[a,] + 0
+        Data.loc[:,'score_ac'].iloc[b,]=Data.loc[:,'score_ac'].iloc[b,] + 1
     else:
         print('Enter a suitable value for "Won"')
 
-    Datos.loc[:,'games'].iloc[a,]=Datos.loc[:,'games'].iloc[a,] + 1
-    Datos.loc[:,'games'].iloc[b,]=Datos.loc[:,'games'].iloc[b,] + 1
+    Data.loc[:,'games'].iloc[a,]=Data.loc[:,'games'].iloc[a,] + 1
+    Data.loc[:,'games'].iloc[b,]=Data.loc[:,'games'].iloc[b,] + 1
 
 def bigger_better(a,b,variable):
     
-    A=Datos.iloc[a,]
-    B=Datos.iloc[b,]
+    A=Data.iloc[a,]
+    B=Data.iloc[b,]
     
     if A[variable] > B[variable]:
         a_against_b(a,b,1)
@@ -82,8 +82,8 @@ def bigger_better(a,b,variable):
 
 def less_better(a,b,variable):
 
-    A=Datos.iloc[a,]
-    B=Datos.iloc[b,]
+    A=Data.iloc[a,]
+    B=Data.iloc[b,]
     
     
     if A[variable] < B[variable]:
@@ -97,7 +97,7 @@ def less_better(a,b,variable):
 
 
 
-Datos=pd.read_csv('H:/2020-02/How to find a new home with Scraping and game theory/Data_Final.csv') 
+Data=pd.read_csv('H:/2020-02/How to find a new home with Scraping and game theory/Data_Final.csv') 
 
 less=['Valor de arriendo', 'Valor de administración','Estrato','Localidad_or']
 bigger=['Área construida','Habitaciones','Baños', 'Parqueadero','Área privada','Negociar Precio']
@@ -108,44 +108,50 @@ Localidad_or=['Usaquén', 'Suba', 'Chapinero','Barrios Unidos','Teusaquillo',
               np.nan,'La Candelaria','Fontibón','Puente Aranda','Engativá', 'Santa Fe','Kennedy','San Cristóbal','Usme','Bosa',
               'Ciudad Bolívar', 'Tunjuelito','Los Mártires','Rafael Uribe Uribe', 'Antonio Nariño']
 
-Datos['Localidad_or']=0
+Data['Localidad_or']=0
 i=0
 for l in Localidad_or:
     i=i+1
-    Datos['Localidad_or']=np.where(Datos['Localidad']==l,i,Datos['Localidad_or'])
+    Data['Localidad_or']=np.where(Data['Localidad']==l,i,Data['Localidad_or'])
 
-Datos['Localidad_or']=np.where(Datos['Localidad'].isnull(),6,Datos['Localidad_or'])    
-Datos['Negociar Precio']=1*Datos['Negociar Precio']
-
-Datos['score']=1500
-Datos['score_ex']=0.0
-Datos['score_ac']=0.0
-Datos['games']=0.0
+Data['Localidad_or']=np.where(Data['Localidad'].isnull(),6,Data['Localidad_or'])    
+Data['Negociar Precio']=1*Data['Negociar Precio']
 
 
-for i in range(len(Datos)*20):
-    a=random.choice(range(len(Datos)))
-    b=random.choice(range(len(Datos)))
+Data['score_fi']=0
+
+for champ in range(10):
     
-    num=random.choice(range(100))
+    Data['score']=1500
+    Data['score_ex']=0.0
+    Data['score_ac']=0.0
+    Data['games']=0.0
+    
+    for i in range(len(Data)*10):
+        a=random.choice(range(len(Data)))
+        b=random.choice(range(len(Data)))
+        
+        num=random.choice(range(100))
+    
+        for j in range(len(weights)):
+            num=num-weights[j]
+            if num<= 0:
+                vari=less+bigger
+                var=vari[j]
+                break
+        # a,b index of the apartments
+        if var in less:
+            less_better(a,b,var)
+        else:
+            bigger_better(a,b,var)
+        pct=round(100*(i+1)/(len(Data)*10+1),2)
+        print(f'-{champ}---------------------- {pct}% ---------------------------' )
+        if i%len(Data)==0:
+            Data['score']=update_rating(Data['score'],34,Data['score_ac'],Data['score_ex'])        
+     
+    Data['score_fi']=Data['score_fi']+Data['score']
+    
+Data['score_fi']=Data['score_fi']/40
 
-    for j in range(len(weights)):
-        num=num-weights[j]
-        if num<= 0:
-            vari=less+bigger
-            var=vari[j]
-            break
-    # a,b index of the apartments
-    if var in less:
-        less_better(a,b,var)
-    else:
-        bigger_better(a,b,var)
-    pct=round(100*(i+1)/(len(Datos)*20+1),2)
-    print(f'--------------------- {pct}% ---------------------------' )
-    if i%len(Datos)==0:
-        Datos['score']=update_rating(Datos['score'],34,Datos['score_ac'],Datos['score_ex'])        
- 
-
-
-
+Data.to_csv('H:/2020-02/How to find a new home with Scraping and game theory/Data_score.csv')
 
